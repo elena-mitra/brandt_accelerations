9535015eb116286dc71e7b843cbed9a7106a24c0
Elena Mitra <notetoelena@me.com>
2019-07-02 17:12:27 -0400

    astrometric acceleration calculation function

---
+++ f_calc_astrometric_acceleration.py
@@ -0,0 +1,30 @@
+import numpy as np
+from astropy import units as u
+
+def calc_astrometric_acceleration(plx,
+                                  pmrahip,
+                                  pmragaia,
+                                  pmrahg,
+                                  pmdechip,
+                                  pmdecgaia,
+                                  pmdechg,
+                                  rahipepoch,
+                                  ragaiaepoch,
+                                  dechipepoch,
+                                  decgaiaepoch):
+    '''returns astrometric acceleration'''
+
+    # Converts parallax to parsec
+    d = plx.to(u.parsec, equivalencies=u.parallax())
+
+    # Calculate accelerations (using d and 4.74 to convert to linear units)
+    acc_ra_num = (pmragaia - pmrahg) * 4.74 * d  # ∆µ (in mas/yr) * parallax (in parsec) converted to (mas pc/yr)
+    acc_ra_den = (ragaiaepoch - rahipepoch) / 2
+    acc_dec_num = (pmdecgaia - pmdechg) * 4.74 * d
+    acc_dec_den = (decgaiaepoch - dechipepoch) / 2
+    acc_ra = acc_ra_num / acc_ra_den
+    acc_dec = acc_dec_num / acc_dec_den
+
+    # Vector add acceleration components, and convert to liner units using 4.74
+    astrometric_acc = np.sqrt(acc_ra ** 2 + acc_dec ** 2)
+    return astrometric_acc
